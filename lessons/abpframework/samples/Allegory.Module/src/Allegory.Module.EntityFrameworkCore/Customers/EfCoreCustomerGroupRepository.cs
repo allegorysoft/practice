488889219593d8d5f81e031a15e4318307582abd
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-06-24 18:34:40 +0300

    EfCoreRepositories added

---
+++ lessons/abpframework/samples/Allegory.Module/src/Allegory.Module.EntityFrameworkCore/Customers/EfCoreCustomerGroupRepository.cs
@@ -0,0 +1,29 @@
+﻿using Allegory.Module.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace Allegory.Module.Customers;
+
+public class EfCoreCustomerGroupRepository : EfCoreRepository<IModuleDbContext, CustomerGroup, Guid>, ICustomerGroupRepository
+{
+    public EfCoreCustomerGroupRepository(IDbContextProvider<IModuleDbContext> dbContextProvider)
+        : base(dbContextProvider)
+    {
+
+    }
+
+    public async Task<CustomerGroup> FindByCodeAsync(
+        string code,
+        bool includeDetails = true,
+        CancellationToken cancellationToken = default)
+    {
+        return await (await GetDbSetAsync())
+            .FirstOrDefaultAsync(cg => cg.Code == code,
+            GetCancellationToken(cancellationToken));
+    }
+}
+
