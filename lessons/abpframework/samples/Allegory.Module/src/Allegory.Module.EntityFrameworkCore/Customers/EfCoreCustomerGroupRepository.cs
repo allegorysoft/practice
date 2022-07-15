efa0593daccc02bd0a2fa5de911e31afd904fc84
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-07-15 15:43:56 +0300

    Customers domain mongo repository added

--- lessons/abpframework/samples/Allegory.Module/src/Allegory.Module.EntityFrameworkCore/Customers/EfCoreCustomerGroupRepository.cs
+++ lessons/abpframework/samples/Allegory.Module/src/Allegory.Module.EntityFrameworkCore/Customers/EfCoreCustomerGroupRepository.cs
@@ -26,4 +26,3 @@ public class EfCoreCustomerGroupRepository : EfCoreRepository<IModuleDbContext, 
             GetCancellationToken(cancellationToken));
     }
 }
-
---
+++ lessons/abpframework/samples/Allegory.Module/src/Allegory.Module.MongoDB/Customers/MongoCustomerGroupRepository.cs
@@ -0,0 +1,28 @@
+﻿using Allegory.Module.MongoDB;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.MongoDB;
+using Volo.Abp.MongoDB;
+
+namespace Allegory.Module.Customers;
+
+public class MongoCustomerGroupRepository : MongoDbRepository<IModuleMongoDbContext, CustomerGroup, Guid>, ICustomerGroupRepository
+{
+    public MongoCustomerGroupRepository(IMongoDbContextProvider<IModuleMongoDbContext> dbContextProvider) : base(dbContextProvider)
+    {
+
+    }
+
+    public virtual async Task<CustomerGroup> FindByCodeAsync(
+        string code,
+        bool includeDetails = true,
+        CancellationToken cancellationToken = default)
+    {
+        return await (await GetMongoQueryableAsync(cancellationToken))
+            .FirstOrDefaultAsync(cg => cg.Code == code,
+            GetCancellationToken(cancellationToken));
+    }
+}
