19b8c13801ba318623e4582807c921b85b6f4b22
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-07-16 15:18:57 +0300

    test added

--- lessons/abpframework/samples/Allegory.SampleMongoApp/test/Allegory.SampleMongoApp.MongoDB.Tests/MongoDb/Samples/SampleRepositoryTests.cs
+++ lessons/abpframework/samples/Allegory.SampleMongoApp/test/Allegory.SampleMongoApp.MongoDB.Tests/MongoDb/Samples/SampleRepositoryTests.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 using Shouldly;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp;
 using Volo.Abp.AuditLogging;
+using Volo.Abp.Data;
 using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Identity;
-using Volo.Abp.TenantManagement;
+using Volo.Abp.MultiTenancy;
 using Xunit;
 
 namespace Allegory.SampleMongoApp.MongoDB.Samples;
@@ -20,11 +23,13 @@ public class SampleRepositoryTests : SampleMongoAppMongoDbTestBase
 {
     private readonly IRepository<IdentityUser, Guid> _appUserRepository;
     private readonly IRepository<AuditLog, Guid> _auditLogRepository;
-    private readonly IRepository<Tenant, Guid> _tenantRepository;
+    private readonly IDataFilter _dataFilter;
 
     public SampleRepositoryTests()
     {
         _appUserRepository = GetRequiredService<IRepository<IdentityUser, Guid>>();
+        _auditLogRepository = GetRequiredService<IRepository<AuditLog, Guid>>();
+        _dataFilter = GetRequiredService<IDataFilter>();
     }
 
     [Fact]
@@ -35,12 +40,48 @@ public class SampleRepositoryTests : SampleMongoAppMongoDbTestBase
          */
         await WithUnitOfWorkAsync(async () =>
         {
-                //Act
-                var adminUser = await (await _appUserRepository.GetMongoQueryableAsync())
-                .FirstOrDefaultAsync(u => u.UserName == "admin");
+            //Act
+            var adminUser = await (await _appUserRepository.GetMongoQueryableAsync())
+            .FirstOrDefaultAsync(u => u.UserName == "admin");
 
-                //Assert
-                adminUser.ShouldNotBeNull();
+            //Assert
+            adminUser.ShouldNotBeNull();
         });
+    }
+
+    [Fact]
+    public async Task Should_Join_Audit_To_User()
+    {
+        using (_dataFilter.Disable<IMultiTenant>())
+        {
+            using (_dataFilter.Disable<ISoftDelete>())
+            {
+                var auditQueryable = await _auditLogRepository.GetMongoQueryableAsync();
+                var userQueryable = await _appUserRepository.GetMongoQueryableAsync();
+
+                var query = from user in userQueryable
+                            join auditLog in auditQueryable on user.Id equals auditLog.UserId.Value
+                            select new { user, auditLog };
+
+                var result = await query.ToListAsync();
+
+                result.ShouldNotBeNull();
+            }
+        }
+    }
+
+    [Fact]
+    public async Task Should_Join_Audit_To_User_With_Collection_Method()
+    {
+        var auditQueryable = await _auditLogRepository.GetCollectionAsync();
+        var userQueryable = await _appUserRepository.GetCollectionAsync();
+
+        var query = from user in userQueryable.AsQueryable()
+                    join auditLog in auditQueryable.AsQueryable() on user.Id equals auditLog.UserId.Value
+                    select new { user, auditLog };
+
+        var result = await query.ToListAsync();
+
+        result.ShouldNotBeNull();
     }
 }
