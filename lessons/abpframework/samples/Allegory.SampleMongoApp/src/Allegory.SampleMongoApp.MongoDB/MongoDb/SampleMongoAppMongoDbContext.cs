9c6b817c519767ca8ceaa42a258a6a9fffd4ccd1
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-07-17 14:53:11 +0300

    MongoDB ReplaceDbContext added

--- lessons/abpframework/samples/Allegory.SampleMongoApp/src/Allegory.SampleMongoApp.MongoDB/MongoDb/SampleMongoAppMongoDbContext.cs
+++ lessons/abpframework/samples/Allegory.SampleMongoApp/src/Allegory.SampleMongoApp.MongoDB/MongoDb/SampleMongoAppMongoDbContext.cs
@@ -1,22 +1,34 @@
-﻿using Volo.Abp.Data;
+﻿using MongoDB.Driver;
+using Volo.Abp.AuditLogging;
+using Volo.Abp.AuditLogging.MongoDB;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Identity;
+using Volo.Abp.Identity.MongoDB;
 using Volo.Abp.MongoDB;
 
 namespace Allegory.SampleMongoApp.MongoDB;
 
 [ConnectionStringName("Default")]
-public class SampleMongoAppMongoDbContext : AbpMongoDbContext
+[ReplaceDbContext(typeof(IAbpIdentityMongoDbContext),typeof(IAuditLoggingMongoDbContext))]
+public class SampleMongoAppMongoDbContext : AbpMongoDbContext,
+    IAbpIdentityMongoDbContext,
+    IAuditLoggingMongoDbContext
 {
-    /* Add mongo collections here. Example:
-     * public IMongoCollection<Question> Questions => Collection<Question>();
-     */
+    public IMongoCollection<IdentityUser> Users => Collection<IdentityUser>();
+    public IMongoCollection<IdentityRole> Roles => Collection<IdentityRole>();
+    public IMongoCollection<IdentityClaimType> ClaimTypes => Collection<IdentityClaimType>();
+    public IMongoCollection<OrganizationUnit> OrganizationUnits => Collection<OrganizationUnit>();
+    public IMongoCollection<IdentitySecurityLog> SecurityLogs => Collection<IdentitySecurityLog>();
+    public IMongoCollection<IdentityLinkUser> LinkUsers => Collection<IdentityLinkUser>();
+
+    public IMongoCollection<AuditLog> AuditLogs => Collection<AuditLog>();
 
     protected override void CreateModel(IMongoModelBuilder modelBuilder)
     {
         base.CreateModel(modelBuilder);
 
-        //builder.Entity<YourEntity>(b =>
-        //{
-        //    //...
-        //});
+        modelBuilder.ConfigureIdentity();
+        modelBuilder.ConfigureAuditLogging();
     }
 }
