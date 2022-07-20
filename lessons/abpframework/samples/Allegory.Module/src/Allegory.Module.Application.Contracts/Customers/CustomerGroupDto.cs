05c2770ff5a9bf64b4e204a6241d8e0c380db465
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-07-20 16:52:20 +0300

    CustomerGroup DTOs created

---
+++ lessons/abpframework/samples/Allegory.Module/src/Allegory.Module.Application.Contracts/Customers/CustomerGroupDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Allegory.Module.Customers;
+
+public class CustomerGroupDto : ExtensibleEntityDto<Guid>
+{
+    public string Code { get; set; }
+    public string Description { get; set; }
+}
