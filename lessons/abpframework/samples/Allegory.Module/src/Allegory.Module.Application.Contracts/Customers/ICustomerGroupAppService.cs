823bc4cadfdf2bdd91e4cc0290d228d0a0ab5959
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-07-24 12:50:50 +0300

    Customers appservice interface added

---
+++ lessons/abpframework/samples/Allegory.Module/src/Allegory.Module.Application.Contracts/Customers/ICustomerGroupAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Allegory.Module.Customers;
+
+public interface ICustomerGroupAppService : IApplicationService
+{
+    Task<CustomerGroupDto> GetAsync(Guid id);
+
+    Task<CustomerGroupDto> GetByCodeAsync(string code);
+
+    Task<PagedResultDto<CustomerGroupDto>> GetListAsync(PagedAndSortedResultRequestDto input);
+
+    Task<CustomerGroupDto> CreateAsync(CustomerGroupCreateUpdateDto input);
+
+    Task<CustomerGroupDto> UpdateAsync(Guid id, CustomerGroupCreateUpdateDto input);
+
+    Task DeleteAsync(Guid id);
+}
