e4d81381f3226d6ed9b66c8f001c1de685e38c59
Masum ULU <masum.ulu@hotmail.com>
2022-07-31 16:25:08 +0300

    removed unused imports

--- lessons/abp-angular/samples/Allegory.NgSampleApp/angular/src/app/app.component.ts
+++ lessons/abp-angular/samples/Allegory.NgSampleApp/angular/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { ABP, eLayoutType, RoutesService, TreeNode } from '@abp/ng.core';
+import { eLayoutType, RoutesService } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
