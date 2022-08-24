c285b08c50d72673726f00243f8b04d33358a2e8
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-08-24 21:44:33 +0300

    Plug-in module reference changed as embedded resource

---
+++ lessons/abpframework/samples/Allegory.Module/plug-in/Allegory.Module.PlugInRandomNames/RuntimeRegisterer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using Volo.Abp.VirtualFileSystem;
+
+public class RuntimeRegisterer : IHostedService
+{
+    protected IVirtualFileProvider VirtualFileProvider { get; }
+
+    public RuntimeRegisterer(IVirtualFileProvider virtualFileProvider)
+    {
+        VirtualFileProvider = virtualFileProvider;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        await Task.CompletedTask;
+    }
+
+    private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+    {
+        if (IsSuffixResource(args.Name)) return null;
+
+        var name = $"{new AssemblyName(args.Name).Name}.dll";
+        if (name == "Bogus.dll")
+        {
+            using (var stream = VirtualFileProvider.GetFileInfo("/Allegory/Module/PlugInRandomNames/MyResources/Bogus.dll").CreateReadStream())
+            {
+                return Assembly.Load(stream.GetAllBytes());
+            }
+        }
+
+        throw new DllNotFoundException(name);
+    }
+
+    private bool IsSuffixResource(string argsName)
+    {
+        string[] fields = argsName.Split(',');
+        string name = fields[0];
+        string culture = fields[2];
+
+        return name.EndsWith(".resources") && !culture.EndsWith("neutral");
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
