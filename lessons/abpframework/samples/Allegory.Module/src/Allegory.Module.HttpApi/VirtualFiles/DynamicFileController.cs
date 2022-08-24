36bcd638a34d5ac118d9c4bcd6a62e93ea6cd972
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-08-24 22:01:24 +0300

    Dynamic file provider added

---
+++ lessons/abpframework/samples/Allegory.Module/src/Allegory.Module.HttpApi/VirtualFiles/DynamicFileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using Volo.Abp;
+using Volo.Abp.VirtualFileSystem;
+
+namespace Allegory.Module.VirtualFiles;
+
+[RemoteService(Name = ModuleRemoteServiceConsts.RemoteServiceName)]
+[Area(ModuleRemoteServiceConsts.ModuleName)]
+[Route("api/module/dynamic-files")]
+public class DynamicFileController : ModuleController
+{
+    protected IDynamicFileProvider DynamicFileProvider { get; }
+
+    public DynamicFileController(IDynamicFileProvider dynamicFileProvider)
+    {
+        DynamicFileProvider = dynamicFileProvider;
+    }
+
+    [HttpGet("add-or-update")]
+    public void AddOrUpdate(string value)
+    {
+        DynamicFileProvider.AddOrUpdate(
+            new InMemoryFileInfo(
+                "/dynamic-files/test.txt",
+                value.GetBytes(),
+                "test.txt"
+            )
+        );
+    }
+}
