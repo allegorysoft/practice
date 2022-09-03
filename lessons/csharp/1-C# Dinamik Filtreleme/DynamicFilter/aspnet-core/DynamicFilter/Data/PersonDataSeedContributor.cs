787df8794c68ccede3e7553ee091858626553c41
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-09-03 18:11:37 +0300

    PersonDataSeedContributor added

---
+++ lessons/csharp/1-C# Dinamik Filtreleme/DynamicFilter/aspnet-core/DynamicFilter/Data/PersonDataSeedContributor.cs
@@ -0,0 +1,48 @@
+﻿using Bogus;
+using DynamicFilter.Entities;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Guids;
+using Person = DynamicFilter.Entities.Person;
+
+namespace DynamicFilter.Data;
+
+public class PersonDataSeedContributor : IDataSeedContributor, ITransientDependency
+{
+    private readonly IRepository<Person, Guid> _personRepository;
+    private readonly IGuidGenerator _guidGenerator;
+
+    public PersonDataSeedContributor(
+        IRepository<Person, Guid> personRepository,
+        IGuidGenerator guidGenerator)
+    {
+        _personRepository = personRepository;
+        _guidGenerator = guidGenerator;
+    }
+
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        var people = new Faker<Person>("tr")
+            .CustomInstantiator(f => new Person(_guidGenerator.Create()))
+            .RuleFor(u => u.Gender, f => f.PickRandom<Gender>())
+            .RuleFor(u => u.Name, (f, u) => f.Name.FirstName(ConvertToBogusGender(u.Gender)))
+            .RuleFor(u => u.Surname, (f, u) => f.Name.LastName(ConvertToBogusGender(u.Gender)))
+            .RuleFor(u => u.BirthDate, f => f.Date.Between(DateTime.Now.Date.AddYears(-50), DateTime.Now.Date))
+            .RuleFor(u => u.Balance, f => f.Finance.Amount(-100_000, 100_000))
+            .Generate(10000);
+
+        await _personRepository.InsertManyAsync(people);
+    }
+
+    private Bogus.DataSets.Name.Gender ConvertToBogusGender(Gender gender)
+    {
+        switch (gender)
+        {
+            case Gender.Male:
+                return Bogus.DataSets.Name.Gender.Male;
+            default:
+                return Bogus.DataSets.Name.Gender.Female;
+        }
+    }
+}
--- lessons/csharp/1-C# Dinamik Filtreleme/DynamicFilter/aspnet-core/DynamicFilter/Entities/Person.cs
+++ lessons/csharp/1-C# Dinamik Filtreleme/DynamicFilter/aspnet-core/DynamicFilter/Entities/Person.cs
@@ -10,23 +10,13 @@ public class Person : BasicAggregateRoot<Guid>
     public Gender Gender { get; set; }
     public decimal Balance { get; set; }
 
-    public Person(
-        Guid id,
-        string name,
-        string surname,
-        DateTime birthDate,
-        Gender gender,
-        decimal balance) : base(id)
+    public Person(Guid id) : base(id)
     {
-        Name = name;
-        Surname = surname;
-        BirthDate = birthDate;
-        Gender = gender;
-        Balance = balance;
+       
     }
 }
 
-public enum Gender : byte
+public enum Gender
 {
     Male,
     Female
