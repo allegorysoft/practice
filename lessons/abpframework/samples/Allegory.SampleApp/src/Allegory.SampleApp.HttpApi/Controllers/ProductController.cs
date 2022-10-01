45fc8986aa3aecec440a9a5dfb4f3f1ea97120ec
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-10-01 16:53:05 +0300

    ProductController added

---
+++ lessons/abpframework/samples/Allegory.SampleApp/src/Allegory.SampleApp.HttpApi/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Allegory.SampleApp.Products;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Allegory.SampleApp.Controllers;
+
+public class ProductController : SampleAppController, IProductAppService
+{
+    protected IProductAppService ProductAppService { get; }
+
+    public ProductController(IProductAppService productAppService)
+    {
+        ProductAppService = productAppService;
+    }
+
+    [HttpGet("is-uow")]
+    public bool IsUow() => ProductAppService.IsUow();
+
+    [HttpGet("is-uow-async")]
+    public Task<bool> IsUowAsync() => ProductAppService.IsUowAsync();
+
+    [HttpGet("virtual-is-uow")]
+    public bool VirtualIsUow() => ProductAppService.VirtualIsUow();
+
+    [HttpGet("virtual-is-uow-async")]
+    public Task<bool> VirtualIsUowAsync() => ProductAppService.VirtualIsUowAsync();
+
+    [HttpGet("multiple-call")]
+    public Task MultipleCall()
+    {
+        ProductAppService.IsUow();
+        ProductAppService.IsUowAsync();
+        ProductAppService.VirtualIsUow();
+        ProductAppService.VirtualIsUowAsync();
+
+        return Task.CompletedTask;
+    }
+}
