0cf19e1cbf9f7df0870f0125e0ff6286905d5ee1
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-10-01 15:57:38 +0300

    Product domain created

---
+++ lessons/abpframework/samples/Allegory.SampleApp/src/Allegory.SampleApp.Application.Contracts/Products/IProductAppService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace Allegory.SampleApp.Products;
+
+public interface IProductAppService : IApplicationService
+{
+    bool IsUow();
+    bool VirtualIsUow();
+    Task<bool> IsUowAsync();
+    Task<bool> VirtualIsUowAsync();
+}
---
+++ lessons/abpframework/samples/Allegory.SampleApp/src/Allegory.SampleApp.Application/Products/ProductAppService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace Allegory.SampleApp.Products;
+
+public class ProductAppService : SampleAppAppService, IProductAppService
+{
+    public bool IsUow()
+    {
+        return !(CurrentUnitOfWork == null);
+    }
+
+    public virtual bool VirtualIsUow()
+    {
+        return !(CurrentUnitOfWork == null);
+    }
+
+    public async Task<bool> IsUowAsync()
+    {
+        return !await Task.FromResult(CurrentUnitOfWork == null);
+    }
+
+    public virtual async Task<bool> VirtualIsUowAsync()
+    {
+        return !await Task.FromResult(CurrentUnitOfWork == null);
+    }
+}
