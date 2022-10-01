fceacb1ea5291f935fbdcda42bf24827c377cfd5
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-10-01 16:38:32 +0300

    ProductManager added

---
+++ lessons/abpframework/samples/Allegory.SampleApp/src/Allegory.SampleApp.Domain/Products/ProductManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Domain.Services;
+using Volo.Abp.Uow;
+
+namespace Allegory.SampleApp.Products;
+
+public class ProductManager : DomainService
+{
+    public IUnitOfWorkManager UnitOfWorkManager { get; }
+    protected IUnitOfWork CurrentUnitOfWork => UnitOfWorkManager?.Current;
+
+    public ProductManager(IUnitOfWorkManager unitOfWorkManager)
+    {
+        UnitOfWorkManager = unitOfWorkManager;
+    }
+
+    public bool IsUow()
+    {
+        return !(CurrentUnitOfWork == null);
+    }
+
+    public virtual bool VirtualIsUow()
+    {
+        return !(CurrentUnitOfWork == null);
+    }
+
+    public async Task<bool> IsUowAsync()
+    {
+        return !await Task.FromResult(CurrentUnitOfWork == null);
+    }
+
+    public virtual async Task<bool> VirtualIsUowAsync()
+    {
+        return !await Task.FromResult(CurrentUnitOfWork == null);
+    }
+
+    [UnitOfWork]
+    public virtual async Task<bool> UowAttribute()
+    {
+        return !await Task.FromResult(CurrentUnitOfWork == null);
+    }
+}
---
+++ lessons/abpframework/samples/Allegory.SampleApp/test/Allegory.SampleApp.Domain.Tests/Products/ProductManager_Tests.cs
@@ -0,0 +1,34 @@
+﻿using Shouldly;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Allegory.SampleApp.Products;
+
+public class ProductManager_Tests : SampleAppDomainTestBase
+{
+    private readonly ProductManager _productManager;
+
+    public ProductManager_Tests()
+	{
+        _productManager = GetRequiredService<ProductManager>();
+    }
+
+    [Fact]
+    public async Task Is_Unit_of_Work()
+    {
+        _productManager.IsUow().ShouldBe(false);
+        _productManager.VirtualIsUow().ShouldBe(false);
+        (await _productManager.IsUowAsync()).ShouldBe(false);
+        (await _productManager.VirtualIsUowAsync()).ShouldBe(false);
+        (await _productManager.UowAttribute()).ShouldBe(true);
+
+        await WithUnitOfWorkAsync(async () =>
+        {
+            _productManager.IsUow().ShouldBe(true);
+            _productManager.VirtualIsUow().ShouldBe(true);
+            (await _productManager.IsUowAsync()).ShouldBe(true);
+            (await _productManager.VirtualIsUowAsync()).ShouldBe(true);
+            (await _productManager.UowAttribute()).ShouldBe(true);
+        });
+    }
+}
