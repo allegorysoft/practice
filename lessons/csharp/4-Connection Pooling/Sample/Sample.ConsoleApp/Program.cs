53c843ceddf89a9ae6bc05fabff60ad97f2b063d
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-11-07 12:58:33 +0300

    PoolingBehaviour added

--- lessons/csharp/4-Connection Pooling/Sample/Sample.ConsoleApp/Program.cs
+++ lessons/csharp/4-Connection Pooling/Sample/Sample.ConsoleApp/Program.cs
@@ -1,8 +1,24 @@
-﻿string connectionString = @"
-Server=(LocalDb)\MSSQLLocalDB;
-Database=ConnectionPool;
-Trusted_Connection=True;
-Pooling=true";
+﻿using System.Data.SqlClient;
 
 
+await PoolingBehaviour();
+Console.ReadLine();
 
+
+async Task PoolingBehaviour()
+{
+    string connectionString = @"Server=(LocalDb)\MSSQLLocalDB;Database=ConnectionPool;Trusted_Connection=True;";
+
+    await ConnectAsync(connectionString);
+    await ConnectAsync(connectionString);
+
+    //Veritabanı bağlantısı aynı olmasına rağmen Server bilgisi "MSSQLLocalDb"(sonu Db şeklinde) güncellendiğinde yeni connection pool oluşturuyor
+    connectionString = @"Server=(LocalDb)\MSSQLLocalDb;Database=ConnectionPool;Trusted_Connection=True;";
+    await ConnectAsync(connectionString);
+}
+
+async Task ConnectAsync(string connectionString)
+{
+    using SqlConnection connection = new SqlConnection(connectionString);
+    await connection.OpenAsync();
+}
