9aaf6a3fe068355a7420e8871c9b33a2399b6bc1
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-12-11 22:51:35 +0300

    Unnecessary using deleted

--- lessons/csharp/5-Değer Tipi ve Referans Tipi Farkı/Sample/Sample.StackVsHeap/StringTopic/StringSample.cs
+++ lessons/csharp/5-Değer Tipi ve Referans Tipi Farkı/Sample/Sample.StackVsHeap/StringTopic/StringSample.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sample.StackVsHeap.StringTopic;
+﻿namespace Sample.StackVsHeap.StringTopic;
 
 internal class StringSample
 {
