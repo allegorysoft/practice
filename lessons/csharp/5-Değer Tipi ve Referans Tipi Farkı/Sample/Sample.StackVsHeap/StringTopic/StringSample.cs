bafa9459bbfaa49ea538dce3e4d0bf8ac037a474
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-12-11 22:51:20 +0300

    StringSample added

---
+++ lessons/csharp/5-Değer Tipi ve Referans Tipi Farkı/Sample/Sample.StackVsHeap/StringTopic/StringSample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.StackVsHeap.StringTopic;
+
+internal class StringSample
+{
+    public static void Do()
+    {
+        string name = "ahmet";
+        name += " faruk";
+        name = "mehmet";
+    }
+}
