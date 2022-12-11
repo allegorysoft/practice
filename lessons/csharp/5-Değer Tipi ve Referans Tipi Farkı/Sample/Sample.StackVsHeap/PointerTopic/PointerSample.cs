b9d7aedbf50bcc5ad6dc6f64dbdf47bdf350c6f0
ahmetfarukulu <ahmetfarukulu@gmail.com>
2022-12-11 22:45:46 +0300

    PointerSample added

---
+++ lessons/csharp/5-Değer Tipi ve Referans Tipi Farkı/Sample/Sample.StackVsHeap/PointerTopic/PointerSample.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace Sample.StackVsHeap.PointerTopic;
+
+internal class PointerSample
+{
+    //https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/unsafe-code
+    //https://learn.microsoft.com/en-us/dotnet/api/system.intptr
+    public static void Do()
+    {
+        PointerSize();
+        MethodParameterConvention();
+        Example();
+    }
+
+    static void PointerSize()
+    {
+        var pointerSize = IntPtr.Size;
+        object o = new object();
+        var list = new List<object>();
+
+        for (int i = 0; i < 100_000_000; i++)
+        {
+            list.Add(o);
+        }
+
+        Console.ReadKey();
+    }
+
+    unsafe static void MethodParameterConvention()
+    {
+        int number = 10;
+        int* pointer = &number;
+        TakeNumber(pointer);
+    }
+    unsafe static void TakeNumber(int* pointer)
+    {
+        *pointer = 20;
+    }
+
+    unsafe static void Example()
+    {
+        var pointer = Marshal.AllocHGlobal(sizeof(int) * 5);
+        int* numberPointer = (int*)pointer.ToPointer();
+
+        for (int i = 0; i < 5; i++)
+        {
+            *(numberPointer + i) = i * 2;
+        }
+
+        Marshal.FreeHGlobal(pointer);
+    }
+}
--- lessons/csharp/5-Değer Tipi ve Referans Tipi Farkı/Sample/Sample.StackVsHeap/Program.cs
+++ lessons/csharp/5-Değer Tipi ve Referans Tipi Farkı/Sample/Sample.StackVsHeap/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Sample.StackVsHeap.HeapTopic;
+using Sample.StackVsHeap.PointerTopic;
 using Sample.StackVsHeap.StackTopic;
 
 StackSample.Do();
 HeapSample.Do();
+PointerSample.Do();
