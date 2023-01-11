8e8ed67c332c8563231b2984438f346b84064c38
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-01-11 16:33:29 +0300

    Sample added

--- lessons/csharp/7-Kutulama ve Kutudan Çıkarma/Sample/Sample.ConsoleApp/Program.cs
+++ lessons/csharp/7-Kutulama ve Kutudan Çıkarma/Sample/Sample.ConsoleApp/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.ReadKey();
+﻿
+Sample1();
+Sample2();
+
+Console.ReadKey();
+
+void Sample1()
+{
+    int i = 10;
+    object o = i;//boxing
+    int j = (int)o;//unboxing
+
+    o = 20;
+    o = 30;
+}
+
+void Sample2()
+{
+    for (int i = 0; i < 100_000_000; i++)
+    {
+        WithObject(i);
+        WithGeneric(i);
+    }
+}
+
+void WithObject(object o)
+{
+
+}
+
+void WithGeneric<T>(T o)
+{
+
+}
