18d26c07c57533c20a56d979ea36c8051b0e12ca
masumulu28 <masum.ulu@hotmail.com>
2023-01-13 21:15:50 +0300

    Created home component

--- lessons/angular/1-NgTemplateOutlet/ng-template-outlet/src/app/app-routing.module.ts
+++ lessons/angular/1-NgTemplateOutlet/ng-template-outlet/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadComponent: () => import('./home/home.component') }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ lessons/angular/1-NgTemplateOutlet/ng-template-outlet/src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html'
+})
+export default class HomeComponent {}
