dc3f267ac6a224065e449ae38f675f50ff943bc0
masumulu28 <masum.ulu@hotmail.com>
2023-01-29 18:01:58 +0300

    angular module updated

--- lessons/abpframework/samples/Allegory.Module/angular/projects/dev-app/src/app/app.module.ts
+++ lessons/abpframework/samples/Allegory.Module/angular/projects/dev-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AccountConfigModule } from '@abp/ng.account/config';
 import { CoreModule } from '@abp/ng.core';
 import { registerLocale } from '@abp/ng.core/locale';
 import { IdentityConfigModule } from '@abp/ng.identity/config';
+import { AbpOAuthModule } from '@abp/ng.oauth';
 import { SettingManagementConfigModule } from '@abp/ng.setting-management/config';
 import { TenantManagementConfigModule } from '@abp/ng.tenant-management/config';
 import { ThemeBasicModule } from '@abp/ng.theme.basic';
@@ -29,6 +30,7 @@ import { APP_ROUTE_PROVIDER } from './route.provider';
     ThemeSharedModule.forRoot(),
     AccountConfigModule.forRoot(),
     IdentityConfigModule.forRoot(),
+    AbpOAuthModule.forRoot(),
     TenantManagementConfigModule.forRoot(),
     SettingManagementConfigModule.forRoot(),
     ModuleConfigModule.forRoot(),
