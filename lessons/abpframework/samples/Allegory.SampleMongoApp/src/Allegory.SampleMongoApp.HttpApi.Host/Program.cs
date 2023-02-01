9c30d6e42db93fa7d54a04e558f0454fdbee09bb
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-02-01 13:38:53 +0300

    Log level changed

--- lessons/abpframework/samples/Allegory.SampleMongoApp/src/Allegory.SampleMongoApp.HttpApi.Host/Program.cs
+++ lessons/abpframework/samples/Allegory.SampleMongoApp/src/Allegory.SampleMongoApp.HttpApi.Host/Program.cs
@@ -14,12 +14,12 @@ public class Program
     {
         Log.Logger = new LoggerConfiguration()
 #if DEBUG
-            .MinimumLevel.Debug()
+            .MinimumLevel.Warning()
 #else
             .MinimumLevel.Information()
 #endif
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+            //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            //.MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
             .Enrich.FromLogContext()
             .WriteTo.Async(c => c.File("Logs/logs.txt"))
             .WriteTo.Async(c => c.Console())
