688139e334a762b0322b1f74d615bd84ddcfe9f0
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-02-13 17:50:58 +0300

    Integration service added

---
+++ lessons/abpframework/samples/Allegory.SampleMongoApp/src/Allegory.SampleMongoApp.Domain/OptionPattern/IIntegrationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace Allegory.SampleMongoApp.OptionPattern;
+
+public interface IIntegrationService : ITransientDependency
+{
+    Task DoAsync();
+}
---
+++ lessons/abpframework/samples/Allegory.SampleMongoApp/src/Allegory.SampleMongoApp.Domain/OptionPattern/XIntegrationService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Volo.Abp.DependencyInjection;
+
+namespace Allegory.SampleMongoApp.OptionPattern;
+
+[DisableConventionalRegistration]
+public class XIntegrationService : IIntegrationService
+{
+    protected virtual SampleOptions Options { get; }
+
+    public XIntegrationService(IOptions<SampleOptions> options)
+    {
+        Options = options.Value;
+    }
+
+    public Task DoAsync()
+    {
+        var username = Options.Username;
+        var password = Options.Password;
+
+        return Task.CompletedTask;
+    }
+}
