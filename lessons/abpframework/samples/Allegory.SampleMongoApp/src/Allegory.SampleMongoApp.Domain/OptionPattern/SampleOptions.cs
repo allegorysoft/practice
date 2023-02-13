b3c2c6962f3bf68fd046a579cecc3a3832c79dff
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-02-13 17:51:25 +0300

    Options added

---
+++ lessons/abpframework/samples/Allegory.SampleMongoApp/src/Allegory.SampleMongoApp.Domain/OptionPattern/SampleOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Allegory.SampleMongoApp.OptionPattern;
+
+public class SampleOptions
+{
+    public const string Position = "Sample";
+
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ lessons/abpframework/samples/Allegory.SampleMongoApp/src/Allegory.SampleMongoApp.Domain/OptionPattern/SamplePreOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Allegory.SampleMongoApp.OptionPattern;
+
+public class SamplePreOptions
+{
+    public string IntegrationService { get; set; }
+    public bool DoOtherThings { get; set; }
+
+    public Type GetIntegrationService()
+    {
+        var type =  typeof(XIntegrationService);
+
+        if (!IntegrationService.IsNullOrWhiteSpace())
+        {
+            type = Type.GetType(IntegrationService) ?? type;
+        }
+
+        return type;
+    }
+}
