ebf9a26f5c9d059380377449c283fc4adf46e042
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-03-23 17:34:15 +0300

    Array creation changed

--- lessons/csharp/8-Span ve stackalloc/Sample/Sample.ConsoleApp/SpanDefinitions.cs
+++ lessons/csharp/8-Span ve stackalloc/Sample/Sample.ConsoleApp/SpanDefinitions.cs
@@ -15,7 +15,7 @@ public class SpanDefinitions
         array2[0] = 500;
         Console.WriteLine("İlk dizi: {0}, İkinci dizi: {1}", array[1], array2[0]);
 
-        int[] arraySpan = new int[] { 1, 2, 3, 4, 5 };
+        int[] arraySpan = Enumerable.Range(1, 5).ToArray();
         Span<int> arraySpan2 = arraySpan.AsSpan()[1..3]; //Diğer yöntem: arraySpan.AsSpan(1,2);
         arraySpan2[0] = 500;
         Console.WriteLine("İlk dizi: {0}, İkinci dizi: {1}", arraySpan[1], arraySpan2[0]);
