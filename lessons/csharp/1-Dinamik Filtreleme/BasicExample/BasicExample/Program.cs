07ce513c0029c2f09b7e38d2d6a3efb2d8edf6d3
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-04-20 15:30:24 +0300

    Description added for requests

--- lessons/csharp/1-Dinamik Filtreleme/BasicExample/BasicExample/Program.cs
+++ lessons/csharp/1-Dinamik Filtreleme/BasicExample/BasicExample/Program.cs
@@ -6,11 +6,11 @@ var app = builder.Build();
 
 var people = new List<Person>
 {
-    new Person("Kelly", "Pitts", new DateTime(1980, 1, 1), Gender.Male, 1500),
-    new Person("Anne", "Henry", new DateTime(1981, 1, 1), Gender.Female, 2000),
-    new Person("Ashton", "Rangel", new DateTime(1982, 1, 1), Gender.Male, 2500),
-    new Person("Charley", "Atkinson", new DateTime(1983, 1, 1), Gender.Female, 3000),
-    new Person("Anne", "Huff", new DateTime(1984, 1, 1), Gender.Female, 3500)
+    new("Jamie", "Pitts", new DateTime(1980, 1, 1), Gender.Male, 1500),
+    new("Anne", "Henry", new DateTime(1981, 1, 1), Gender.Female, 2000),
+    new("Ashton", "Rangel", new DateTime(1982, 1, 1), Gender.Male, 2500),
+    new("Charley", "Atkinson", new DateTime(1983, 1, 1), Gender.Female, 3000),
+    new("Anne", "Huff", new DateTime(1984, 1, 1), Gender.Female, 3500)
 };
 
 app.MapPost("/person/list",
