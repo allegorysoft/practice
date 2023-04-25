b64ee67e649330dc5e32c46d8ad1a98f2d5cea2c
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-04-25 11:55:12 +0300

    InterlockedExample added

--- lessons/csharp/12-Thread Synchronization ve Race Condition/Sample/Sample.ConsoleApp/Program.cs
+++ lessons/csharp/12-Thread Synchronization ve Race Condition/Sample/Sample.ConsoleApp/Program.cs
@@ -1,11 +1,41 @@
-﻿Problem();
+﻿InterlockedExample();
+//RaceConditionProblem();
 
 Console.ReadKey();
 
-void Problem()
+void InterlockedExample()
+{
+    var number = 0;
+    var interlockedNumber = 0;
+
+    ThreadStart work = () =>
+    {
+        for (var i = 0; i < 1_000_000; i++)
+        {
+            number++;
+            Interlocked.Increment(ref interlockedNumber);//Decrement, Add, Exchange
+        }
+    };
+
+    var threads = new Thread[Environment.ProcessorCount];
+    for (var i = 0; i < threads.Length; i++)
+    {
+        threads[i] = new Thread(work);
+        threads[i].Start();
+    }
+
+    foreach (var thread in threads)
+    {
+        thread.Join();
+    }
+
+    Console.WriteLine($"Number: {number} InterlockedNumber: {interlockedNumber}");
+}
+
+void RaceConditionProblem()
 {
     var money = 100;
-    ParameterizedThreadStart work = (object? m) =>
+    ParameterizedThreadStart work = m =>
     {
         var expense = (int)m!;
         if (money >= expense)
