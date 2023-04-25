eb8ea6a43707ab1092c1a8ba03faa0faf3d01630
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-04-25 13:10:26 +0300

    Sample methods refactored

---
+++ lessons/csharp/12-Thread Synchronization ve Race Condition/Sample/Sample.ConsoleApp/LockAndMonitor.cs
@@ -0,0 +1,84 @@
+﻿namespace Sample.ConsoleApp;
+
+public class LockAndMonitor
+{
+    private static int _money = 100;
+    private static readonly object LockObject = new object();
+
+    public static void Do()
+    {
+        WithLock();
+        WithMonitor();
+    }
+
+    private static void WithLock()
+    {
+        var t1 = new Thread(() => TryPaymentWithLock(75));
+        var t2 = new Thread(() => TryPaymentWithLock(75));
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine(_money);
+    }
+
+    private static void TryPaymentWithLock(int expense)
+    {
+        lock (LockObject)
+        {
+            //Critical section
+            if (_money >= expense)
+            {
+                //await Task.Delay(1000); !!!
+                Thread.Sleep(1000);
+                _money -= expense;
+            }
+            else
+            {
+                Console.WriteLine("Expense payment denied.");
+            }
+        }
+    }
+
+    private static void WithMonitor()
+    {
+        var t1 = new Thread(() => TryPaymentWithMonitor(75));
+        var t2 = new Thread(() => TryPaymentWithMonitor(75));
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine(_money);
+    }
+
+    private static void TryPaymentWithMonitor(int expense)
+    {
+        var lockTaken = false;
+        try
+        {
+            //Critical section
+            Monitor.Enter(LockObject, ref lockTaken);
+            if (_money >= expense)
+            {
+                //await Task.Delay(1000); !!!
+                Thread.Sleep(1000);
+                _money -= expense;
+            }
+            else
+            {
+                Console.WriteLine("Expense payment denied.");
+            }
+        }
+        finally
+        {
+            if (lockTaken)
+                Monitor.Exit(LockObject);
+        }
+    }
+}
--- lessons/csharp/12-Thread Synchronization ve Race Condition/Sample/Sample.ConsoleApp/Program.cs
+++ lessons/csharp/12-Thread Synchronization ve Race Condition/Sample/Sample.ConsoleApp/Program.cs
@@ -1,93 +1,9 @@
-﻿//InterlockedExample();
-//RaceConditionProblem();
-//LockAndMonitor();
+﻿using Sample.ConsoleApp;
+
+//InterlockedSample.Do();
+//RaceCondition.Do();
+//LockAndMonitor.Do();
 
 Console.ReadKey();
 
-void InterlockedExample()
-{
-    var number = 0;
-    var interlockedNumber = 0;
 
-    ThreadStart work = () =>
-    {
-        for (var i = 0; i < 1_000_000; i++)
-        {
-            number++;
-            Interlocked.Increment(ref interlockedNumber); //Decrement, Add, Exchange
-        }
-    };
-
-    var threads = new Thread[Environment.ProcessorCount];
-    for (var i = 0; i < threads.Length; i++)
-    {
-        threads[i] = new Thread(work);
-        threads[i].Start();
-    }
-
-    foreach (var thread in threads)
-    {
-        thread.Join();
-    }
-
-    Console.WriteLine($"Number: {number} InterlockedNumber: {interlockedNumber}");
-}
-
-void RaceConditionProblem()
-{
-    var money = 100;
-    ParameterizedThreadStart work = m =>
-    {
-        var expense = (int)m!;
-        if (money >= expense)
-        {
-            Thread.Sleep(1000);
-            money -= expense;
-        }
-        else
-        {
-            Console.WriteLine("Expense payment denied.");
-        }
-    };
-
-    var t1 = new Thread(work);
-    var t2 = new Thread(work);
-    t1.Start(75);
-    t2.Start(75);
-    t1.Join();
-    t2.Join();
-
-    Console.WriteLine(money);
-}
-
-void LockAndMonitor()
-{
-    var money = 100;
-    var lockObject = new object();
-    ParameterizedThreadStart work = async m =>
-    {
-        var expense = (int)m!;
-        lock (lockObject)
-        {
-            //Critical section
-            if (money >= expense)
-            {
-                Thread.Sleep(1000); //await Task.Delay(1000); !!!
-                money -= expense;
-            }
-            else
-            {
-                Console.WriteLine("Expense payment denied.");
-            }
-        }
-    };
-
-    var t1 = new Thread(work);
-    var t2 = new Thread(work);
-    t1.Start(75);
-    t2.Start(75);
-    t1.Join();
-    t2.Join();
-
-    Console.WriteLine(money);
-}
