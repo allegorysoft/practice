223324f3d4502947c7ee9956d462a1bb1c357b63
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-04-25 11:28:44 +0300

    Problem defined

--- lessons/csharp/12-Thread Synchronization ve Race Condition/Sample/Sample.ConsoleApp/Program.cs
+++ lessons/csharp/12-Thread Synchronization ve Race Condition/Sample/Sample.ConsoleApp/Program.cs
@@ -1,2 +1,31 @@
-﻿
+﻿Problem();
+
 Console.ReadKey();
+
+void Problem()
+{
+    var money = 100;
+    ParameterizedThreadStart work = (object? m) =>
+    {
+        var expense = (int)m!;
+        if (money >= expense)
+        {
+            Thread.Sleep(1000);
+            money -= expense;
+        }
+        else
+        {
+            Console.WriteLine("Expense payment denied.");
+        }
+    };
+
+    var t1 = new Thread(work);
+    var t2 = new Thread(work);
+    t1.Start(75);
+    t2.Start(75);
+    t1.Join();
+    t2.Join();
+
+    Console.WriteLine(money);
+}
+
