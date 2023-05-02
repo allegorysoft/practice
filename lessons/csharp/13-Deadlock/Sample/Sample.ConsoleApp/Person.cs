1a1eff9614212ea0b0b5f24d37b1ad5e26760e56
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-05-02 18:30:01 +0300

    DeadLockSample added

---
+++ lessons/csharp/13-Deadlock/Sample/Sample.ConsoleApp/Person.cs
@@ -0,0 +1,44 @@
+﻿namespace Sample.ConsoleApp;
+
+public class Person
+{
+    private static int _counter = 0;
+
+    public int Id { get; }
+    public int Balance { get; protected set; } = 500;
+
+    public Person()
+    {
+        Interlocked.Increment(ref _counter);
+        Id = _counter;
+    }
+
+    public void Transfer(int amount, Person to)
+    {
+        object lockObject1 = Id > to.Id ? this : to;
+        object lockObject2 = lockObject1 == this ? to : this;
+
+        lock (lockObject1)
+        {
+            Thread.Sleep(1000);
+            lock (lockObject2)
+            {
+                Balance -= amount;
+                to.Balance += amount;
+            }
+        }
+    }
+
+    public void TransferDeadLock(int amount, Person to)
+    {
+        lock (this)
+        {
+            Thread.Sleep(1000);
+            lock (to)
+            {
+                Balance -= amount;
+                to.Balance += amount;
+            }
+        }
+    }
+}
--- lessons/csharp/13-Deadlock/Sample/Sample.ConsoleApp/Program.cs
+++ lessons/csharp/13-Deadlock/Sample/Sample.ConsoleApp/Program.cs
@@ -1,2 +1,19 @@
-﻿
+﻿using Sample.ConsoleApp;
+
+DeadLockSample();
 Console.ReadKey();
+
+void DeadLockSample()
+{
+    var ahmet = new Person();
+    var mehmet = new Person();
+
+    var t1 = new Thread(() => ahmet.TransferDeadLock(100, mehmet));
+    var t2 = new Thread(() => mehmet.TransferDeadLock(100, ahmet));
+    t1.Start();
+    t2.Start();
+    t1.Join();
+    t2.Join();
+
+    Console.WriteLine("Aktarım tamamlandı");
+}
