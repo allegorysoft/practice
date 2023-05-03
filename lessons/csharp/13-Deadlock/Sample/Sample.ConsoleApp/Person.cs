2b2a992f22c83ad171422abbf193046831c477e8
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-05-03 11:31:42 +0300

    LiveLock method added

--- lessons/csharp/13-Deadlock/Sample/Sample.ConsoleApp/Person.cs
+++ lessons/csharp/13-Deadlock/Sample/Sample.ConsoleApp/Person.cs
@@ -2,15 +2,47 @@
 
 public class Person
 {
-    private static int _counter = 0;
+    private static int _counter;
 
     public int Id { get; }
     public int Balance { get; protected set; } = 500;
 
     public Person()
     {
-        Interlocked.Increment(ref _counter);
-        Id = _counter;
+        Id = Interlocked.Increment(ref _counter);
+    }
+
+    public void TransferDeadLock(int amount, Person to)
+    {
+        lock (this)
+        {
+            Thread.Sleep(1000);
+            lock (to)
+            {
+                Balance -= amount;
+                to.Balance += amount;
+            }
+        }
+    }
+
+    public void TransferLiveLock(int amount, Person to)
+    {
+        lock (this)
+        {
+            Thread.Sleep(1000);
+            try
+            {
+                while (!Monitor.TryEnter(to))
+                    Thread.Sleep(1000);
+
+                Balance -= amount;
+                to.Balance += amount;
+            }
+            finally
+            {
+                Monitor.Exit(to);
+            }
+        }
     }
 
     public void Transfer(int amount, Person to)
@@ -22,19 +54,6 @@ public class Person
         {
             Thread.Sleep(1000);
             lock (lockObject2)
-            {
-                Balance -= amount;
-                to.Balance += amount;
-            }
-        }
-    }
-
-    public void TransferDeadLock(int amount, Person to)
-    {
-        lock (this)
-        {
-            Thread.Sleep(1000);
-            lock (to)
             {
                 Balance -= amount;
                 to.Balance += amount;
