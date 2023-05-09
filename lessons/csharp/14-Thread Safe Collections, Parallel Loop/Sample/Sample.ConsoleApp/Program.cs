24e87fe5f59fabf1164e4354d1ec23d3cdbb767f
ahmetfarukulu <ahmetfarukulu@gmail.com>
2023-05-09 17:47:28 +0300

    BlockingCollectionSample added

--- lessons/csharp/14-Thread Safe Collections, Parallel Loop/Sample/Sample.ConsoleApp/Program.cs
+++ lessons/csharp/14-Thread Safe Collections, Parallel Loop/Sample/Sample.ConsoleApp/Program.cs
@@ -1,2 +1,27 @@
-﻿
+﻿using System.Collections.Concurrent;
+
+BlockingCollectionSample();
 Console.ReadKey();
+
+void BlockingCollectionSample()
+{
+    var collection = new BlockingCollection<int>(1);
+    new Thread(() =>
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            collection.Add(i);
+            Console.WriteLine($"Producer Added: {i}");
+        }
+    }).Start();
+
+    new Thread(() =>
+    {
+        while (collection.TryTake(out var item, Timeout.Infinite))
+        {
+            Console.WriteLine($"Consumer Start: {item}");
+            Thread.Sleep(5_000);
+            Console.WriteLine($"Consumer End: {item}");
+        }
+    }).Start();
+}
